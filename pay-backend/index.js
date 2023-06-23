7066571c81fee8e1f0fe394910ba935c5bb970bc
Akshay7623 <merakshay2000@gmail.com>
2023-06-23 10:08:05 +0530

    first commit

---
+++ pay-backend/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+require('./config.js');
+const express  = require('express');
+const cors = require('cors');
+const path = require('path');
+const getPayDetailsRoute = require('./routes/getPayDetailsRoute.js');
+const submitPayDetailsRoute = require('./routes/submitPayDetailsRoute.js');
+const RazorpayWebhook = require('./routes/razorpay-webhook.js');
+
+
+
+const port = 5300;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/getdetails',getPayDetailsRoute);
+app.use('/api/submitpay',submitPayDetailsRoute);
+app.use('/api/razorpay-webhook',RazorpayWebhook);
+
+app.use(express.static(__dirname + '/build'));
+app.get("/*", (req, res) => {
+   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port,()=>{
+    console.log(`app is running on port ${port}`);
+});
